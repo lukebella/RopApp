896a40cb9f53d5c5d65ae3bb10691e8343560113
Julián Jesús Alba Sineiro <julian.jesus.alba@udc.es>
2024-04-04 21:23:45 +0200

    Borrada la barra de navegacion de create outfit

--- RopApp/app/src/main/java/com/apm/ropapp/ui/calendar/CalendarFragment.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/ui/calendar/CalendarFragment.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.RecyclerView
-import com.apm.ropapp.R
 import com.apm.ropapp.databinding.FragmentCalendarBinding
-import com.apm.ropapp.ui.closet.CustomAdapter
 
 class CalendarFragment : Fragment() {
     private var _binding: FragmentCalendarBinding? = null
