546eddcf9df3907977fa8a83f531fb02aa8de87c
lukebella <bellaniluca9@gmail.com>
2024-04-05 16:49:03 +0200

    Add implementation and permissions for localization

--- RopApp/app/build.gradle.kts
+++ RopApp/app/build.gradle.kts
@@ -48,6 +48,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    implementation("com.google.android.gms:play-services-location:22.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
