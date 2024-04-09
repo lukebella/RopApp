998352b1d05ae0b614e11ef701f0ae20157d666e
hugo-alvarez-sm <h.sanmiguel@udc.es>
2024-04-09 23:54:29 +0200

    Configurado e implementado Inicio de sesión con email y password en Firebase. Implementada creación de usuarios.

--- RopApp/app/build.gradle.kts
+++ RopApp/app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics:21.6.1")
     implementation("com.google.firebase:firebase-database:20.3.1")
     implementation("com.google.firebase:firebase-firestore-ktx:24.11.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
 
     implementation("com.google.android.gms:play-services-location:21.2.0")
 
--- RopApp/app/src/main/java/com/apm/ropapp/Login.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/Login.kt
@@ -4,21 +4,40 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.apm.ropapp.databinding.LoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class Login : AppCompatActivity() {
     private lateinit var binding: LoginBinding
-
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth= FirebaseAuth.getInstance()
 
         binding.loginButton.setOnClickListener() {
             Log.d("Login", "Se hizo clic en Iniciar sesión")
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+
+            val email = binding.userLogin.text.toString()
+            val password = binding.userPassword.text.toString()
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener()
+                {
+                    if(it.isSuccessful){
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    }else{
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }else{
+                Toast.makeText(this, "Debe rellenar todos los campos", Toast.LENGTH_SHORT).show()
+            }
         }
 
         binding.signupButton.setOnClickListener() {
