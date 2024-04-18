011f76ec1a779a43ef644a24a128a24d1eb6e4db
hugo-alvarez-sm <h.sanmiguel@udc.es>
2024-04-18 21:10:49 +0200

    Comienzo de implementacion de funcionalidad de EditProfile

--- RopApp/app/src/main/java/com/apm/ropapp/EditProfile.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/EditProfile.kt
@@ -7,15 +7,43 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.apm.ropapp.databinding.EditprofileBinding
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class EditProfile : AppCompatActivity() {
     private lateinit var binding: EditprofileBinding
+    private lateinit var databaseReference: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = EditprofileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+        // Obtener una referencia a la base de datos de Firebase para los datos del user actual
+        databaseReference = userId?.let {
+            FirebaseDatabase.getInstance().reference.child("users").child(it) }!!
+
+        databaseReference.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Verificar si los datos existen en la base de datos
+                if (dataSnapshot.exists()) {
+                    // Obtener los datos del usuario actual
+                    val username = dataSnapshot.child("username").getValue(String::class.java)
+
+                    // Llenar los campos de EditText con la información del usuario actual
+                    username?.let { binding.editUsername.setText(it) }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Manejar la cancelación del evento de obtención de datos
+            }
+        })
 
         // Initialize your views here
         // For example, to set a click listener on the floating action button:
@@ -48,9 +76,9 @@ class EditProfile : AppCompatActivity() {
             }
         }
 
-        binding.editEmail.setOnFocusChangeListener { v, hasFocus ->
+        binding.editGender.setOnFocusChangeListener { v, hasFocus ->
             if (!hasFocus) {
-                val email = binding.editEmail.text.toString()
+                val email = binding.editGender.text.toString()
                 Log.d("EditProfile", "Email: $email")
                 // You can now use the email variable
             }
@@ -61,8 +89,6 @@ class EditProfile : AppCompatActivity() {
             Log.d("EditProfile", "Change Password")
             startActivity(intent)
         }
-
-
 
         // Similarly, initialize other views and set their behavior
     }
