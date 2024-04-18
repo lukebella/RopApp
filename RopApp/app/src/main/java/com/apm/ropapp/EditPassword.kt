7a38ae936e172659a022647ca7929e8123489faf
hugo-alvarez-sm <h.sanmiguel@udc.es>
2024-04-18 20:43:39 +0200

    Añadida funcion de Cambio de Contraseña en EditPassword

--- RopApp/app/src/main/java/com/apm/ropapp/EditPassword.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/EditPassword.kt
@@ -1,13 +1,17 @@
 package com.apm.ropapp
 
-import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.apm.ropapp.databinding.EditpasswordBinding
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.EmailAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import org.checkerframework.checker.nullness.qual.NonNull
 
 
 class EditPassword : AppCompatActivity() {
@@ -18,6 +22,7 @@ class EditPassword : AppCompatActivity() {
         binding = EditpasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var newPassword = ""
+        var oldPassword = ""
 
         // Initialize your views here
         // For example, to set a click listener on the floating action button:
@@ -29,18 +34,25 @@ class EditPassword : AppCompatActivity() {
         binding.saveButton3.setOnClickListener {
             Log.d("EditPassword", "Change Password")
 
-            val user = Firebase.auth.currentUser
-            val passwd = newPassword
+            val user = FirebaseAuth.getInstance().currentUser
+            val email: String? = user?.email
+            val credential = email?.let { it1 -> EmailAuthProvider.getCredential(it1, oldPassword) }
 
-            user!!.updatePassword(passwd)
+            user!!.reauthenticate(credential!!)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        Log.d(TAG, "User password updated.")
+                        user.updatePassword(newPassword)
+                            .addOnCompleteListener { task ->
+                                if (!task.isSuccessful) {
+                                    Toast.makeText(this, "Something went wrong. Please try again later", Toast.LENGTH_SHORT).show()
+                                } else {
+                                    Toast.makeText(this, "Password Successfully Modified", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                    } else {
+                        Toast.makeText(this, "Authentication Failed", Toast.LENGTH_SHORT).show()
                     }
                 }
-
-
-
             intent = Intent(this, MainActivity::class.java) // Maybe editOutfit?
             intent.putExtra("fragment", R.id.navigation_user)
             startActivity(intent)
@@ -54,7 +66,13 @@ class EditPassword : AppCompatActivity() {
             }
         }
 
+        binding.oldPassword.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                oldPassword = binding.oldPassword.text.toString()
+                Log.d("EditPassword", "Old Password: $oldPassword")
+                // You can now use the newPassword variable
+            }
+        }
 
-        // Similarly, initialize other views and set their behavior
     }
 }
