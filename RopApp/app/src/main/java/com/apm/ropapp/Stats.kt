ec833acc775a6bee08359cf496b700bf47ef300b
Ale <a.estebanm@udc.es>
2024-05-11 11:55:24 +0200

    Adding Stats to the profile:
    - Added the connection to the DB
    - Added the type and season charts
    - Still in development

--- RopApp/app/src/main/java/com/apm/ropapp/Stats.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/Stats.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.apm.ropapp.databinding.StatsBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class Stats : AppCompatActivity() {
     private lateinit var binding: StatsBinding
@@ -16,6 +21,23 @@ class Stats : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = StatsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val userId = FirebaseAuth.getInstance().currentUser?.uid
+        if (userId != null) {
+            FirebaseDatabase.getInstance().reference.child("users")
+                .child(userId).addValueEventListener(object : ValueEventListener {
+
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists())
+                            binding.textView6.text =
+                                dataSnapshot.child("username").getValue(String::class.java)
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        Log.d("ProfileFragment", "Failed to get user data.", databaseError.toException())
+                    }
+                })
+        }
 
         binding.seasonButton.setOnClickListener {
             intent = Intent(this, StatsSeason::class.java)
@@ -33,5 +55,6 @@ class Stats : AppCompatActivity() {
             Log.d("Stats", "Back to MainActivity")
             finish()
         }
+
     }
 }
--- RopApp/app/src/main/java/com/apm/ropapp/StatsType.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/StatsType.kt
@@ -4,7 +4,7 @@ import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.apm.ropapp.databinding.StatsSeasonBinding
+import com.apm.ropapp.databinding.StatsTypeBinding
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
@@ -15,82 +15,70 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
-data class SeasonData(
-    val season: String,
-    val clothesCount: Float,
-    val outfitCount: Float
+data class ClothesTypeData(
+    val type: String,
+    val count: Float
 )
 
-class StatsSeason : AppCompatActivity() {
+class StatsType : AppCompatActivity() {
 
-    private lateinit var binding: StatsSeasonBinding
-    // Variable for our bar chart
+    private lateinit var binding: StatsTypeBinding
     private lateinit var barChart: BarChart
-
-    // Variable for our bar data.
     private lateinit var barData: BarData
-
-    // Variable for our bar data set.
     private lateinit var barDataSet: BarDataSet
-
-    // ArrayList for storing entries.
     private lateinit var barEntriesArrayList: ArrayList<BarEntry>
 
     override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    binding = StatsSeasonBinding.inflate(layoutInflater)
-    setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        binding = StatsTypeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    // Initializing variable for bar chart.
-    barChart = binding.seasonChart
+        barChart = binding.typesChart
 
-    // Call the function to get data from the database
-    get_database_data()
+        get_database_data()
 
-    binding.backButton.setOnClickListener {
-        finish()
+        binding.backButton.setOnClickListener {
+            finish()
+        }
     }
-}
 
-    private fun getBarEntries(seasonDataList: List<SeasonData>) {
+    private fun getBarEntries(clothesTypeDataList: List<ClothesTypeData>) {
         barEntriesArrayList = ArrayList()
 
-        for ((index, seasonData) in seasonDataList.withIndex()) {
-            // Use the index as the x value and the clothesCount as the y value
-            barEntriesArrayList.add(BarEntry(index.toFloat(), seasonData.clothesCount))
+        for ((index, clothesTypeData) in clothesTypeDataList.withIndex()) {
+            barEntriesArrayList.add(BarEntry(index.toFloat(), clothesTypeData.count))
         }
 
-        // After getting the entries, update the chart
         updateChart()
     }
 
     private fun updateChart() {
-        barDataSet = BarDataSet(barEntriesArrayList, "Season Data")
+        barDataSet = BarDataSet(barEntriesArrayList, "Clothes Type Data")
         barData = BarData(barDataSet)
         barChart.data = barData
         barDataSet.colors = ColorTemplate.COLORFUL_COLORS.asList()
         barDataSet.valueTextColor = Color.BLACK
         barDataSet.valueTextSize = 16f
         barChart.description.isEnabled = true
-        barChart.invalidate() // refresh the chart
+        barChart.invalidate()
     }
 
     private fun get_database_data() {
         val databaseReference = FirebaseDatabase.getInstance().reference.child("path_to_your_data")
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                val seasonDataList = mutableListOf<SeasonData>()
-                for (seasonSnapshot in dataSnapshot.children) {
-                    val seasonData = seasonSnapshot.getValue(SeasonData::class.java)
-                    if (seasonData != null) {
-                        seasonDataList.add(seasonData)
+                val clothesTypeDataList = mutableListOf<ClothesTypeData>()
+                for (typeSnapshot in dataSnapshot.children) {
+                    val clothesTypeData = typeSnapshot.getValue(ClothesTypeData::class.java)
+                    if (clothesTypeData != null) {
+                        clothesTypeDataList.add(clothesTypeData)
                     }
                 }
-                getBarEntries(seasonDataList)
+                getBarEntries(clothesTypeDataList)
             }
 
             override fun onCancelled(databaseError: DatabaseError) {
-                Log.d("StatsSeason", "Failed to get season data.", databaseError.toException())
+                Log.d("ClothesTypeChart", "Failed to get clothes type data.", databaseError.toException())
             }
         })
     }
