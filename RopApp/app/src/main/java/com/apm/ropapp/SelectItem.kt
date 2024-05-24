bd1ee29196d8e7535dbf2a479560ecb7769cbf3c
Julián Jesús Alba Sineiro <julian.jesus.alba@udc.es>
2024-05-24 21:11:14 +0200

    Actualizadas las opciones de categorias y estilos para las prendas

--- RopApp/app/src/main/java/com/apm/ropapp/SelectItem.kt
+++ RopApp/app/src/main/java/com/apm/ropapp/SelectItem.kt
@@ -3,14 +3,12 @@ package com.apm.ropapp
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.apm.ropapp.databinding.SelectItemBinding
 
 class SelectItem : AppCompatActivity() {
 
     private lateinit var binding: SelectItemBinding
-    private lateinit var buttonSelected: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,35 +22,5 @@ class SelectItem : AppCompatActivity() {
             startActivity(intent)
         }
 
-        binding.floatingAddButton.setOnClickListener {
-            intent = Intent(this, MainActivity::class.java)
-            Log.d("SelectItem","Continue to Main Activity")
-            startActivity(intent)
-        }
-
-        buttonSelected = binding.button1
-        buttonSelected.isSelected = true
-
-        binding.button1.setOnClickListener {
-            selectButton(binding.button1)
-        }
-
-        binding.button2.setOnClickListener {
-            selectButton(binding.button2)
-        }
-
-        binding.button3.setOnClickListener {
-            selectButton(binding.button3)
-        }
-
-        binding.button4.setOnClickListener {
-            selectButton(binding.button4)
-        }
-    }
-
-    private fun selectButton(newButton: Button) {
-        buttonSelected.isSelected = false
-        newButton.isSelected = true
-        buttonSelected = newButton
     }
 }
